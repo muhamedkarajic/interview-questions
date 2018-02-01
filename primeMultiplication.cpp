143e410c079c9a39c2811bf119af0d54a06d7a55
muhamedkarajic <mountgraph@gmail.com>
2018-02-02 00:15:22 +0100

    Update
    A few new exercises.

---
+++ primeMultiplication.cpp
@@ -0,0 +1,76 @@
+﻿//
+//  Created by Muhamed Karajic on December 4, 2017.
+//  Copyright © 2017 Muhamed Karajic. All rights reserved.
+//
+
+/*Napišite program, koji omogućava da unese prirodni number number i u slučaju
+da number nije prime (number koji je djeljiv samo sa 1 i sa samim sobom)
+potrebno ga je rastaviti na proste faktore. Svaki složeni number se može rastaviti
+na proste faktore (numbereve) koji množenjem daju taj number.
+
+48=2*2*2*2*3
+100=2*2*5*5
+60=2*2*3*5*/
+
+//Wikipedia: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+
+#include <iostream>
+using namespace std;
+
+bool prime(int number)
+{
+	if (number == 2 || number == 3 || number == 5 || number == 7)
+		return true;
+
+	else if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0)
+		return false;
+
+	else
+	{
+		int root = sqrt(number);
+		for (int i = 5; i <= root; i += 6)
+		{
+			if (number%i == 0)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void output(int number) {
+	for (int i = 2; number>1; i++)
+	{
+		if (prime(i) == true) {
+			while (number%i == 0)
+			{
+				cout << i << " ";
+				number /= i;
+			}
+		}
+	}
+}
+
+int main() {
+	int number;
+
+	cout << "Enter a number: ";
+	cin >> number;
+
+	if (prime(number) == true) 
+		cout << "This is a prime number." << endl;
+
+	else
+		output(number);
+	
+	cout << endl;
+	system("PAUSE");
+	return 0;
+}
+
+/*
+I would like to thank here Selimović Adela for helping me understand the 
+prime number logic behind this code, which was her optimization and math logic.
+*/
---
+++ transposeMatrix.cpp
@@ -0,0 +1,63 @@
+﻿//
+//  Created by Muhamed Karajic on January 17, 2018.
+//  Copyright © 2018 Muhamed Karajic. All rights reserved.
+//
+
+
+/*
+Napisati program koji će omogućiti:
+•	Unos 2D niza od 10x10 elemanata vodeći računa da su svi elementi dvocifreni (ukoliko unos nekog elementa ne zadovoljava uslov, ponavljati unos tog elementa dok se ne zadovolji uslov) – Koristiti funkciju unos
+•	Izvršiti transpoziciju niza tako što će se zamjeniti redovi i kolone – Koristiti funkciju transpose, a zatim na osnovu izmijenjenog 2D niza:
+	•	Izračunati aritmetičku sredinu svih prostih brojeva ispod sporedne dijagonale – Koristiti dvije funkcije: aritmeticka i prost_broj (pozivati će se iz funkcije aritmeticka)
+	•	Napisati funkciju simpatican koja će provjeriti da li su brojevi iznad sporedne dijagonale simpatični*.
+*/
+
+#include <iostream>
+using namespace std;
+
+const int length = 5;
+
+void print(int array[][length])
+{
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void transpose(int array[][length])
+{
+	int temp;
+	for (int i = 0; i<length; i++) 
+	{
+		for (int j = 0; j!=i; j++) 
+		{
+				temp = array[i][j];
+				array[i][j] = array[j][i];
+				array[j][i] = temp;
+		}
+	}
+}
+
+int main()
+{
+	int array[length][length] = {
+							10,11,12,13,14,
+							20,21,22,23,24,
+							30,31,32,33,34,
+							40,41,42,43,44,
+							50,51,52,53,54
+						};
+	
+	print(array);
+	transpose(array);
+	print(array);
+
+	system("PAUSE");
+	return 0;
+}
