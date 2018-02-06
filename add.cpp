0383353f242e9d61f0f605bf42e5473ef1031812
muhamedkarajic <mountgraph@gmail.com>
2018-02-07 00:21:47 +0100

    Update

---
+++ add.cpp
@@ -0,0 +1,19 @@
+/*
+Write a function that returns the sum of two numbers.
+
+Example
+
+For param1 = 1 and param2 = 2, the output should be
+add(param1, param2) = 3.
+*/
+
+int add(int param1, int param2) {
+    return param1+param2;
+}
+
+
+
+//
+//  Created by Muhamed Karajic on January 4, 2017.
+//  Copyright © 2017 Muhamed Karajic. All rights reserved.
+//
