810c8e3adbdf36f840c1173c4ac8768ec09f2bf5
muhamedkarajic <mountgraph@gmail.com>
2018-02-15 00:00:24 +0100

    Update 3 February 14, 2018

--- alternatingSums.cpp
+++ alternatingSums.cpp
@@ -1,7 +1,10 @@
 /*
-Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+Several people are standing in a row and need to be divided into two teams. 
+The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
 
-You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+You are given an array of positive integers - the weights of the people. 
+Return an array of two integers, where the first element is the total weight of team 1, 
+and the second element is the total weight of team 2 after the division is complete.
 
 Example
 
