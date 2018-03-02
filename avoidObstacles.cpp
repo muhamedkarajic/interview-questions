048c6360b5e473175b70d6fdcb86069813578694
muhamedkarajic <mountgraph@gmail.com>
2018-03-03 00:30:39 +0100

    Update 4 on March 3, 2018.

--- avoidObstacles.cpp
+++ avoidObstacles.cpp
@@ -1,7 +1,8 @@
 /*
 You are given an array of integers representing coordinates of obstacles situated on a straight line.
 
-Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+Assume that you are jumping from the point with coordinate 0 to the right. 
+You are allowed only to make jumps of the same length represented by some integer.
 
 Find the minimal length of the jump enough to avoid all the obstacles.
 
