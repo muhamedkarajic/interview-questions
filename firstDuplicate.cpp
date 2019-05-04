62e09decab1906927300ef2fce085a8e2776dbcd
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2019-05-04 04:05:27 +0200

    Create firstDuplicate.cpp

---
+++ firstDuplicate.cpp
@@ -0,0 +1,23 @@
+// Given an array a that contains only numbers in the range from 1 to a.length, 
+//find the first duplicate number for which the second occurrence has the minimal index. 
+//In other words, if there are more than 1 duplicated numbers, 
+//return the number for which the second occurrence has a smaller index than the second occurrence 
+//of the other number does. If there are no such elements, return -1.
+
+// Example
+
+//     For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+
+//     There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than 
+//     the second occurrence of 2 does, so the answer is 3.
+
+int firstDuplicate(std::vector<int> a) {
+    unordered_set<int> t;
+    int index = INT_MAX;
+    for(int i = 0; i < a.size(); i++)
+        if (t.find(a[i]) != t.end())
+            return a[i];
+        else
+            t.insert(a[i]);
+    return -1;
+}
