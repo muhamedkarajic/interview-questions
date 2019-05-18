9532c5b90b9d3d5b575b648148a443fd91ccd9c0
Muhamed Karajic <mountgraph@gmail.com>
2019-05-18 16:15:12 +0200

    Update absoluteValuesSumMinimization.cpp

--- absoluteValuesSumMinimization.cpp
+++ absoluteValuesSumMinimization.cpp
@@ -1,8 +1,8 @@
 
 // Given a sorted array of integers a, your task is to determine which element of a is closest to all other values of a. 
-//In other words, find the element x in a, which minimizes the following sum:
+// In other words, find the element x in a, which minimizes the following sum:
 
-//abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+// abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
 
 
 int absoluteValuesSumMinimization(std::vector<int> a) {
